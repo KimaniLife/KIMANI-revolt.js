99a8739779fe2c67dacedbe3e865ddd8d9b30282
SanchezrEdwin01 <sanchez.r.edwin09@gmail.com>
2026-01-07 13:50:05 -0600

    Marketplace

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,30 @@
-export * as API from "revolt-api";
-export { Client } from "./Client.js";
-export type { ClientOptions, Session as PrivateSession } from "./Client.js";
-export * from "./classes/index.js";
-export * from "./collections/index.js";
-export { ConnectionState, EventClient } from "./events/index.js";
+export { Channel } from "./maps/Channels";
+export { Member } from "./maps/Members";
+export { Message } from "./maps/Messages";
+export { Server } from "./maps/Servers";
+export { User } from "./maps/Users";
+export { Emoji } from "./maps/Emojis";
+
+export * from "./Client";
+export * from "./config";
+
 export {
-  BotFlags,
-  ServerFlags,
-  UserBadges,
-  UserFlags,
-} from "./hydration/index.js";
-export * from "./lib/regex.js";
+    UserPermission,
+    Permission,
+    DEFAULT_PERMISSION,
+    DEFAULT_PERMISSION_DIRECT_MESSAGE,
+    DEFAULT_PERMISSION_SAVED_MESSAGES,
+    DEFAULT_PERMISSION_SERVER,
+    DEFAULT_PERMISSION_VIEW_ONLY,
+    U32_MAX,
+} from "./permissions/definitions";
+export { calculatePermission } from "./permissions/calculator";
+
+export { Nullable, toNullable, toNullableDate } from "./util/null";
+export {
+    ReadyPacket,
+    ClientboundNotification,
+    ServerboundNotification,
+} from "./websocket/notifications";
+
+export * as API from "revolt-api";
